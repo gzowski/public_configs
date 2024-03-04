8fa0f0999f1fd2ee48fc93fd69bf4c5d401c4ca8
James Gzowski <flexyjerkov@gmail.com>
2024-03-04 14:31:37 +0000

    Delete .config/polybar directory

--- .config/polybar/spotify-music.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
- 
-if [ "$(playerctl --player=spotify metadata --format "{{ artist }} - {{ title }}" >>/dev/null 2>&1; echo $?)" == "1" ]; then
-    echo ""
-else
-    playerctl --player=spotify metadata --format "{{ artist }} - {{ title }}"
-fi
